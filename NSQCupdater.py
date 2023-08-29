e8bb7b6a6b2b195cd75623edebf396a6efb02087
Vladgobelen <vladgobelen@gmail.com>
2023-08-29 13:07:44 +0700

    реализация без экстерминатуса папки Вт 29 авг 2023 13:07:44 +07

--- NSQCupdater.py
+++ NSQCupdater.py
@@ -18,15 +18,16 @@ for root, dirs, files in os.walk(os.getenv('APPDATA')): #читаем рекур
                         data = open(os.path.join(root, file), 'rb').read().decode('windows-1251').replace('\x00', '') #переводим кодировку текста файла в ascii null byte
                         rez = regex.findall(data) #ищем регексп выше
                         if rez: #если нашли, то
-                            if os.path.isdir(rez[0] + '\\Interface\\AddOns\\NSQC'): #если аддон существует, то
-                                shutil.rmtree(rez[0] + '\\Interface\\AddOns\\NSQC') #удаляем аддон
 
                             urllib.request.urlretrieve("https://github.com/Vladgobelen/NSQC/archive/refs/heads/main.zip", "main.zip") #качаем архив с аддоном
                             archive = 'main.zip' #название скачанного архива
                             with zipfile.ZipFile(archive, 'r') as zip_file: #проверка на открытие файла
                                 zip_file.extractall(rez[0] + '\\Interface\\AddOns') #распаковать архив в аддоны сируса
 
-                            os.rename(rez[0] + '\\Interface\\AddOns\\NSQC-main', rez[0] + '\\Interface\\AddOns\\NSQC') #переименовтаь распакованную папку в NSQC
+                            shutil.copytree(rez[0] + '\\Interface\\AddOns\\NSQC-main', rez[0] + '\\Interface\\AddOns\\NSQC',dirs_exist_ok=True) #скопировать файлы в нужное место
+                            if os.path.isdir(rez[0] + '\\Interface\\AddOns\\NSQC-main'): #если аддон существует, то
+                                shutil.rmtree(rez[0] + '\\Interface\\AddOns\\NSQC-main') #удаляем аддон
+
 if os.path.isfile('main.zip'): #если скачанный архив существует возле апдейтера, то
     os.remove('main.zip') #далить архив
 input('Аддон успешно обновлен') #вывести в консоли надпись "Аддон успешно обновлен" с запросом на нажатие любой кнопки клавиатуры

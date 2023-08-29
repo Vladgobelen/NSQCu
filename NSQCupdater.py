9dd1db2f5cc06364c4a42df032657aa7a43f5f4a
Vladgobelen <vladgobelen@gmail.com>
2023-08-29 08:00:26 +0700

    полностью новый апдейтер1 Вт 29 авг 2023 08:00:26 +07

--- NSQCupdater.py
+++ NSQCupdater.py
@@ -7,28 +7,28 @@ import subprocess
 import json
 import ast
 
-for root, dirs, files in os.walk(os.getenv('APPDATA')):
-    for dir in dirs:
-        if 'Sirus' in dir:
-            path = os.path.join(root, dir)
-            for root, dirs, files in os.walk(path):
-                for file in files:
-                    if file.endswith('.log'):
-                        regex = re.compile(r'"client":{"folder":"([^"]+)"')
-                        data = open(os.path.join(root, file), 'rb').read().decode('windows-1251').replace('\x00', '')
-                        rez = regex.findall(data)
-                        if rez:
-                            if os.path.isdir(rez[0] + '\\Interface\\AddOns\\NSQC'):
-                                shutil.rmtree(rez[0] + '\\Interface\\AddOns\\NSQC')
+for root, dirs, files in os.walk(os.getenv('APPDATA')): #читаем рекурсивно каталог %AppData%
+    for dir in dirs: #ищем каталоги
+        if 'Sirus' in dir: #если в каталоге есть слово Сирус
+            path = os.path.join(root, dir) #путь = путь к этому каталогу
+            for root, dirs, files in os.walk(path): #читаем рекурсивно все каталоги в этом пути
+                for file in files: #читаем рекурсивно все файлы в этом пути
+                    if file.endswith('.log'): #если расширение файла .log то:
+                        regex = re.compile(r'"client":{"folder":"([^"]+)"') #ищем начало пути к вовке (регексп)
+                        data = open(os.path.join(root, file), 'rb').read().decode('windows-1251').replace('\x00', '') #переводим кодировку текста файла в ascii null byte
+                        rez = regex.findall(data) #ищем регексп выше
+                        if rez: #если нашли, то
+                            if os.path.isdir(rez[0] + '\\Interface\\AddOns\\NSQC'): #если аддон существует, то
+                                shutil.rmtree(rez[0] + '\\Interface\\AddOns\\NSQC') #удаляем аддон
 
-                            urllib.request.urlretrieve("https://github.com/Vladgobelen/NSQC/archive/refs/heads/main.zip", "main.zip")
-                            archive = 'main.zip'
-                            with zipfile.ZipFile(archive, 'r') as zip_file:
-                                zip_file.extractall(rez[0] + '\\Interface\\AddOns')
+                            urllib.request.urlretrieve("https://github.com/Vladgobelen/NSQC/archive/refs/heads/main.zip", "main.zip") #качаем архив с аддоном
+                            archive = 'main.zip' #название скачанного архива
+                            with zipfile.ZipFile(archive, 'r') as zip_file: #проверка на открытие файла
+                                zip_file.extractall(rez[0] + '\\Interface\\AddOns') #распаковать архив в аддоны сируса
 
-                            os.rename(rez[0] + '\\Interface\\AddOns\\NSQC-main', rez[0] + '\\Interface\\AddOns\\NSQC')
-if os.path.isfile('main.zip'):
-    os.remove('main.zip')
-input('Аддон успешно обновлен')
+                            os.rename(rez[0] + '\\Interface\\AddOns\\NSQC-main', rez[0] + '\\Interface\\AddOns\\NSQC') #переименовтаь распакованную папку в NSQC
+if os.path.isfile('main.zip'): #если скачанный архив существует возле апдейтера, то
+    os.remove('main.zip') #далить архив
+input('Аддон успешно обновлен') #вывести в консоли надпись "Аддон успешно обновлен" с запросом на нажатие любой кнопки клавиатуры
 
 

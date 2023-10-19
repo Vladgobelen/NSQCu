f371ad8e4f539045cf44b829fa65e09b85c1d179
Vladgobelen <vladgobelen@gmail.com>
2023-10-19 19:46:50 +0700

    Чт 19 окт 2023 19:46:50 +07

--- run.py
+++ run.py
@@ -8,17 +8,27 @@ import os.path
 import shlex
 import time
 
+print('1')
 def process_exists(process_name):
+	print('2')
 	progs = str(subprocess.check_output('tasklist'))
 	if process_name in progs:
 		return True
 	else:
 		return False
 
+print(process_exists('Wow.exe'))
+print('3')
 def update():
+	print(process_exists('Wow.exe'))
+	print('4')
 	f = urllib.request.urlopen('https://github.com/Vladgobelen/NSQC/blob/main/vers').read().decode('utf-8').strip()
 	file = open('Interface/AddOns/NSQC/vers', 'r')
+	print(process_exists('Wow.exe'))
+	print('5')
 	if not file.readline().strip() in f:
+		print(process_exists('Wow.exe'))
+		print('6')
 		if os.path.isdir('temp'):
 			shutil.rmtree('temp/')
 		urllib.request.urlretrieve("https://github.com/Vladgobelen/NSQC/archive/refs/heads/main.zip", "main.zip")
@@ -26,6 +36,7 @@ def update():
 		with zipfile.ZipFile(archive, 'r') as zip_file:
 			zip_file.extractall("temp")
 		file_source = 'temp/NSQC-main/'
+		print(file_source)
 		file_destination = 'Interface/AddOns/NSQC/'
 		if not os.path.exists(file_destination):
 			os.mkdir(file_destination)
@@ -38,14 +49,20 @@ def update():
 	file.close()
 	del f
 
+print(process_exists('Wow.exe'))
+print('7')
 update()
 
 if not process_exists('Wow.exe'):
-	subprocess.run("Wow.exe")
-
+	os.startfile("Wow.exe")
+print('8')
 while True:
+	print('9')
 	if process_exists('Wow.exe'):
 		update()
+		print('11')
 		time.sleep(10)
+		print(process_exists('Wow.exe'))
+		print('12')
 	else:
 		break

4700104e14826aec52c5eb1d637d07f1990a439a
Vladgobelen <vladgobelen@gmail.com>
2023-11-02 02:08:08 +0700

    Чт 02 ноя 2023 02:08:08 +07

--- run.py
+++ run.py
@@ -8,9 +8,7 @@ import os.path
 import shlex
 import time
 
-print('1')
 def process_exists(process_name):
-	print('2')
 	progs = str(subprocess.check_output('tasklist'))
 	if process_name in progs:
 		return True
@@ -18,17 +16,36 @@ def process_exists(process_name):
 		return False
 
 print(process_exists('Wow.exe'))
-print('3')
 def update():
 	print(process_exists('Wow.exe'))
-	print('4')
 	f = urllib.request.urlopen('https://github.com/Vladgobelen/NSQC/blob/main/vers').read().decode('utf-8').strip()
 	file = open('Interface/AddOns/NSQC/vers', 'r')
 	print(process_exists('Wow.exe'))
-	print('5')
+	if not os.path.exists('Data/ruRU/patch-ruRU-M.MPQ'):
+		print('Применение патчей. Может занять какое то время.')
+		urllib.request.urlretrieve("https://hub.mos.ru/vladgobelen/nsqcmap/-/raw/main/patch-ruRU-M.MPQ", "patch-ruRU-M.MPQ")
+		os.replace("patch-ruRU-M.MPQ", "Data/ruRU/patch-ruRU-M.MPQ")
+		urllib.request.urlretrieve("https://github.com/Trimitor/WDM-addons/archive/refs/heads/main.zip", "main.zip")
+		if os.path.isdir('temp'):
+			shutil.rmtree('temp/')
+		archive = 'main.zip'
+		with zipfile.ZipFile(archive, 'r') as zip_file:
+			zip_file.extractall("temp")
+		file_source = 'temp/WDM-addons-main/WDM/'
+		file_destination = 'Interface/AddOns/WDM/'
+		if not os.path.exists(file_destination):
+			os.mkdir(file_destination)
+		get_files = os.listdir(file_source)
+		shutil.copytree(file_source, file_destination,dirs_exist_ok=True)
+		if os.path.isfile('main.zip'):
+			os.remove('main.zip')
+		if os.path.isdir('temp'):
+			shutil.rmtree('temp/')
 	if not file.readline().strip() in f:
 		print(process_exists('Wow.exe'))
-		print('6')
+		print('ОБНОВЛЯЮ!!!')
+		print('ОБНОВЛЯЮ!!!')
+		print('ОБНОВЛЯЮ!!!')
 		if os.path.isdir('temp'):
 			shutil.rmtree('temp/')
 		urllib.request.urlretrieve("https://github.com/Vladgobelen/NSQC/archive/refs/heads/main.zip", "main.zip")
@@ -50,19 +67,14 @@ def update():
 	del f
 
 print(process_exists('Wow.exe'))
-print('7')
 update()
 
 if not process_exists('Wow.exe'):
 	os.startfile("Wow.exe")
-print('8')
 while True:
-	print('9')
 	if process_exists('Wow.exe'):
 		update()
-		print('11')
 		time.sleep(10)
 		print(process_exists('Wow.exe'))
-		print('12')
 	else:
 		break
